4823c49586db57b3a7ee81819e1368d3b04a4bfa
felexkemboi <felokemboi10@gmail.com>
2018-03-12 01:08:50 +0300

    add tests with recent and old questions

--- polls/tests.py
+++ polls/tests.py
@@ -14,4 +14,14 @@ class QuestionModelTests(TestCase):
          future_question = Question(pub_date = time)
          self.assertIs(future_question.was_published_recently(),False) 
 
-# Create your tests here.
+     def  test_was_published_recently_with_old_question(self):
+     	 """"was published recently returns False for Qquestions whose pub_date is older than 1 day"""
+     	 time = timezone.now() - datetime.timedelta(days=1,seconds = 1)
+     	 old_question = Question(pub_date = time)
+     	 self.assertIs(old_question.was_published_recently(),False)
+
+     def test_was_published_recently_with_recent_question(self):
+     	 """"was published_recently() returns True for Questions whose pub_date is within the last day"""
+     	 time = timezone.now() - datetime.timedelta(hrs=23 ,mins=59,seconds= 59)
+     	 recent_question = Question(pub_date= time)
+     	 self.assertIs(recent_question.was_published_recently(),True)
